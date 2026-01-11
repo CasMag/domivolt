a77659b5b0a991ed0e14aff58ceb37d5d90d0339
Casimito <mirito.casi@hotmail.com>
2026-01-10 23:48:46 -0400

    update logo y imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   output: 'static',
+   
+   site:
+   'https://domivolt.pages.dev',
+
+   base:'/'
+});
